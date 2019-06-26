1fb446d38e2c50c3396e3e7ec7e4eded0cb18bed
ladejesus <luisito.dejesus@gmail.com>
2019-06-25 20:17:20 -0400

    first commit

---
+++ Sportsblog.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sportsblog.Tests
+{
+   public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_Hello_World()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.Equal("Hello world", result);
+        }
+
+    }
+}
