505d4527b1ef4418116fa174e26e787007d76f49
Sergio Rodriguez <rsergio98@yahoo.com>
2019-06-28 12:00:13 -0400

    Added Repos, SiteContext, hard coded Tags and Sports

---
+++ Sportsblog/SiteContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sportsblog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportsblog
+{
+    public class SiteContext : DbContext
+    {
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Sports> Sports { get; set; }
+        public DbSet<Tags> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=MusicTesting;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            {
+                modelBuilder.Entity<Sports>().HasData(
+
+                    new Sports { SportName = "Soccer", SportId = 1 },
+
+                    new Sports { SportName = "Basketball", SportId = 2});
+
+
+
+                modelBuilder.Entity<Tags>().HasData(
+
+                    new Tags { TagName = "Spoiler", TagId = 1 },
+
+                    new Tags { TagName = "Hot Take", TagId = 2 },
+
+                    new Tags { TagName = "World Cup", TagId = 3 });
+
+
+            }
+
+        }
+
+    }
+
