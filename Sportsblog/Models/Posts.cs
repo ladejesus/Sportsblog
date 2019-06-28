5685bea874abff6460a5289f1e63296a01de312b
Sergio Rodriguez <rsergio98@yahoo.com>
2019-06-28 00:21:44 -0400

    added Models, Controllers, IRepo, NuGet packages for EF

---
+++ Sportsblog/Models/Posts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportsblog.Models
+{
+    public class Posts
+    {
+        public string Title { get; set; }
+        public int PostId { get; set; }
+        public string PostContent { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public string Tags { get; set; }
+        public int SportId { get; set; }
+    }
+}
---
+++ Sportsblog/Models/Tags.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportsblog.Models
+{
+    public class Tags
+    {
+        public string TagName { get; set; }
+        public string Posts { get; set;  }
+    }
+}
