2a422ec1ae8ddaba18fed1618a15bdc91c7bfc92
ladejesus <luisito.dejesus@gmail.com>
2019-06-29 10:06:31 -0400

    re did images migration

---
+++ Sportsblog/Migrations/20190629140434_secondImages.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sportsblog.Migrations
+{
+    public partial class secondImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
